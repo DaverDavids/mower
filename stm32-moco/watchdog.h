bdcd1da03384e33c640223c89e9d95ca69c52f69
DaverDavid <daverdavids@gmail.com>
2026-06-18 16:54:41 -0400

    delete and reset

--- stm32-moco/watchdog.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <stdbool.h>
-
-void watchdog_init(uint32_t timeout_ms);
-void watchdog_feed(void);      // call on every valid command received
-bool watchdog_expired(void);   // returns true if timeout elapsed
-void watchdog_reset(void);
