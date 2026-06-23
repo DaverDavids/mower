3736cda8f37c6c06fe214df43820349c180d7f59
DaverDavid <daverdavids@gmail.com>
2026-06-22 20:36:37 -0400

    rm moco.txt

--- stm32-moco/STM32CubeIDE/Release/Application/User/Core/stm32f1xx_it.d
+++ stm32-moco/STM32CubeIDE/Release/Application/User/Core/stm32f1xx_it.d
@@ -28,7 +28,8 @@ Application/User/Core/stm32f1xx_it.o: \
  ../../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
  ../../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
  ../../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
- ../../Core/Inc/stm32f1xx_it.h
+ ../../Core/Inc/stm32f1xx_it.h ../../Core/Inc/motor_ctrl.h \
+ ../../Core/Inc/main.h
 ../../Core/Inc/main.h:
 ../../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../../Core/Inc/stm32f1xx_hal_conf.h:
@@ -58,3 +59,5 @@ Application/User/Core/stm32f1xx_it.o: \
 ../../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
 ../../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
 ../../Core/Inc/stm32f1xx_it.h:
+../../Core/Inc/motor_ctrl.h:
+../../Core/Inc/main.h:
