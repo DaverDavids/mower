50ef4dca670d8e9a5795be8719475dd479c4ba14
DaverDavid <daverdavids@gmail.com>
2026-06-28 15:49:39 -0400

    remove debug folder

--- stm32-moco/Core/Inc/motor_ctrl.h
+++ stm32-moco/Core/Inc/motor_ctrl.h
@@ -15,7 +15,7 @@
  */
 
 /* Firmware version – displayed in TUI title bar and VERSION command */
-#define FIRMWARE_VERSION  "moco v1.0"
+#define FIRMWARE_VERSION  "moco v1.1"
 
 /* Number of supported motors */
 #define MOTOR_COUNT  3U
